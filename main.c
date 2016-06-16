1f3a82da537d7d4142606b70ee1d6831a229ed83
XrXr <xrsquared@gmail.com>
2016-06-15 23:10:28 -0400

    Boring white triangle drawn with opengl 3.3

--- main.c
+++ main.c
@@ -1,10 +1,52 @@
+#include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <stdio.h>
 
+const float g_vertex_buffer_data[] = {
+    0.75f, 0.75f, 0.0f, 1.0f,
+    0.75f, -0.75f, 0.0f, 1.0f,
+    -0.75f, -0.75f, 0.0f, 1.0f,
+};
+
+const char* vertex_shader =
+"#version 330\n"
+
+"layout(location = 0) in vec4 position;"
+"void main()"
+"{"
+"    gl_Position = position;"
+"}";
+
+const char* frag_shader =
+"#version 330\n"
+
+"out vec4 outputColor;"
+"void main()"
+"{"
+"   outputColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);"
+"}";
+
+GLuint shader_program;
+GLuint pos_buffer_obj;
+
+float last = 0;
+
 void draw(void) {
-    glClearColor(0.0f,0.0f,0.0f,1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
+
+    glUseProgram(shader_program);
+
+    glBindBuffer(GL_ARRAY_BUFFER, pos_buffer_obj);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableVertexAttribArray(0);
+    glUseProgram(0);
+
+    glutSwapBuffers();
 }
 
 void handle_key(unsigned char key, int x, int y) {
@@ -12,16 +54,105 @@ void handle_key(unsigned char key, int x, int y) {
     if (key == 17 && glutGetModifiers() == GLUT_ACTIVE_CTRL)
         glutLeaveMainLoop();
 }
+void changeSize(int w, int h) {
+    glViewport(0, 0, w, h);
+}
+
+GLuint compile_shader(GLenum eShaderType, const char* shader_source) {
+    GLuint shader = glCreateShader(eShaderType);
+    glShaderSource(shader, 1, &shader_source, NULL);
+
+    glCompileShader(shader);
+
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        GLint info_length;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &info_length);
+
+        GLchar* info = malloc(sizeof(GLchar) * (info_length + 1));
+        glGetShaderInfoLog(shader, info_length, NULL, info);
+
+        fprintf(stderr, "Shader compilation failure :\n%s\n", info);
+        free(info);
+    }
+
+    return shader;
+}
+
+GLuint make_shader_program(int n, ...) {
+    va_list vl, vl_detach;
+    size_t i;
+    GLuint program = glCreateProgram();
+
+    va_start(vl, n);
+    va_copy(vl_detach, vl);
+
+    for (i = 0; i < n; i++)
+        glAttachShader(program, va_arg(vl, GLuint));
+
+    glLinkProgram(program);
+
+    GLint status;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE) {
+        GLint info_length;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &info_length);
+
+        GLchar* info = malloc(sizeof(GLchar) * (info_length + 1));
+        glGetProgramInfoLog(program, info_length, NULL, info);
+        fprintf(stderr, "Linker failure: %s\n", info);
+        free(info);
+    }
+
+    for (i = 0; i < n; i++)
+        glDetachShader(program, va_arg(vl, GLuint));
+
+    va_end(vl);
+    va_end(vl_detach);
+
+    return program;
+}
 
 int main(int argc, char **argv) {
+    glutInitContextVersion(3, 3);
+    glewExperimental = GL_TRUE;
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE);
+    glutInitWindowSize(320, 320);
     glutCreateWindow("Fantasy Golf Simulator");
 
-    glutDisplayFunc(draw);
-    glutKeyboardFunc(handle_key);
-    glutFullScreen();
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+        return 1;
+    }
 
+    glutInitDisplayMode(GLUT_RGBA|GLUT_SINGLE|GLUT_MULTISAMPLE);
+    glutDisplayFunc(draw);
+    glutIdleFunc(draw);
+    glutReshapeFunc(changeSize);
+    glutKeyboardFunc(handle_key);
+
+    // glutSetOption(GLUT_MULTISAMPLE, 16);
+    // glEnable(GL_MULTISAMPLE);
+    // glHint(GL_MULTISAMPLE_FILTER_HINT_NV, GL_NICEST);
+
+    GLuint vs = compile_shader(GL_VERTEX_SHADER, vertex_shader);
+    GLuint fs = compile_shader(GL_FRAGMENT_SHADER, frag_shader);
+    shader_program = make_shader_program(2, vs, fs);
+
+    glGenBuffers(1, &pos_buffer_obj);
+
+    glBindBuffer(GL_ARRAY_BUFFER, pos_buffer_obj);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
+                 g_vertex_buffer_data, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+ //   glutFullScreen();
     glutMainLoop();
     return 0;
 }
