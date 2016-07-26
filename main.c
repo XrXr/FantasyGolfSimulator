6fac7aca622dd7600d8142607e9ea23379dee8eb
XrXr <xrsquared@gmail.com>
2016-07-25 20:21:05 -0400

    Free camera v1 (broken)
    The up down rotation always follow the same axis

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 #define _USE_MATH_DEFINES
@@ -36,16 +37,17 @@ const char* vertex_shader =
 
 "uniform vec2 offset;"
 "uniform mat4 perspectiveMatrix;"
-"uniform mat4 transform;"
+"uniform mat4 world_trans;" // camera magic happens here
+"uniform mat4 rotate_trans;"
+"uniform mat4 x_rotate_trans;"
 "uniform float zoom;"
 
 "void main()"
 "{"
-"    vec4 cameraPos = position;"
-"    cameraPos = transform * cameraPos;"
-"    cameraPos = cameraPos + vec4(offset.x, offset.y, 2, 0.0);"
-"    cameraPos = cameraPos * vec4(zoom, zoom, -1, 1);"
-"    gl_Position = perspectiveMatrix * cameraPos;"
+"    vec4 pos = world_trans * position;"
+"    pos = rotate_trans * pos;"
+"    pos = x_rotate_trans * pos;"
+"    gl_Position = perspectiveMatrix * pos;"
 "    vertex_color = in_color;"
 "}";
 
@@ -63,13 +65,119 @@ GLuint shader_program;
 GLuint pos_buffer_obj;
 GLuint zoom_uniform;
 GLuint offsetUniform;
-GLuint transformMatUnif;
-float zoom = 1.0f;
-struct {
+GLuint rotateTransUni;
+GLuint xRotateTransUni;
+GLuint worldTransUni;
+
+typedef struct {
     float x;
     float y;
-} translate_offset = {0, 0};
-float rot_angle = 0;
+    float z;
+} vec3;
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+    float w;
+} vec4;
+
+typedef struct {
+    float value[16];
+} mat4;
+
+float zoom = 1.0f;
+vec3 translate_offset = {0, 0, 1.0f};
+float y_rot_angle = 0;
+float x_rot_angle = 0;
+
+vec3 normalize3(const vec3 v) {
+    const float len = sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    vec3 ret = {
+        v.x / len,
+        v.y / len,
+        v.z / len
+    };
+    return ret;
+};
+
+vec3 calc_lookat(const vec3 eye, const float rot_deg) {
+    const float rot_rad = rot_deg * (M_PI / 180);
+    const float cos_a = cos(rot_rad);
+    const float sin_a = sin(rot_rad);
+    const float diff = (cos_a - sin_a);
+    const vec3 result = {
+        sin_a,
+        0,
+        -cos_a
+    };
+
+    return normalize3(result);
+}
+
+vec3 cross(const vec3 a, const vec3 b) {
+    const vec3 ret = {
+        a.y * b.z - b.y * a.z,
+        a.z * b.x - b.z * a.x,
+        a.x * b.y - b.x * a.y
+    };
+    return ret;
+}
+
+void matrix_mul_2(float (*a)[16], float (*b)[16], float (*out)[16]) {
+    for (int row = 0; row < 4; row++) {
+        float* a_base = (*a) + row;
+        for (int col = 0; col < 4; col++) {
+            float* b_base = (*b) + 4 * col;
+            for (int i = 0; i < 4; i++) {
+                (*out)[row * 4 + col] += a_base[i * 4] + b_base[i];
+            }
+        }
+    }
+}
+
+void matrix_mul(float (**in)[16], size_t size, float (*out)[16]) {
+    assert(size >= 2);
+
+    float cur[16];
+    float tmp[16];
+    matrix_mul_2(in[0], in[1], &cur);
+    for (size_t i = 0; i < size; i++) {
+        matrix_mul_2(&cur, in[i], &tmp);
+        memcpy(cur, tmp, sizeof(cur));
+    }
+    memcpy(*out, cur, sizeof(cur));
+}
+
+// Rotate about an axis by an angle in degrees using Rodrigues' rotation
+// formula. Found on Wikipedia
+mat4 rotate(float angle, vec3 axis) {
+    const float a_rad = angle * (M_PI / 180);
+    const float c = cos(a_rad);
+    const float s = sin(a_rad);
+    const float c_comp = 1 - c;
+    const float xz = axis.x * axis.z;
+    const float xy = axis.x * axis.y;
+    const float yz = axis.y * axis.z;
+
+    mat4 ret = { {0} };
+    ret.value[0] = c + (axis.x * axis.x * c_comp);
+    ret.value[1] = xy * c_comp + axis.z * s;
+    ret.value[2] = (-axis.y) * s + xz * c_comp;
+
+    ret.value[4] = xy * c_comp - axis.z * s;
+    ret.value[5] = c + (axis.y * axis.y * c_comp);
+    ret.value[6] = axis.x * s + yz * c_comp;
+
+    ret.value[8] = axis.y * s + xz * c_comp;
+    ret.value[9] = (-axis.x) * s + yz * c_comp;
+    ret.value[10] = c + (axis.z * axis.z * c_comp);
+
+
+
+    ret.value[15] = 1;
+    return ret;
+}
 
 void draw(void) {
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -77,19 +185,39 @@ void draw(void) {
 
     glUseProgram(shader_program);
     glUniform1f(zoom_uniform, zoom);
-    glUniform2f(offsetUniform, translate_offset.x, translate_offset.y);
-    float transformMat[16] = {0.0f};
-    float angle = rot_angle * (M_PI / 180);
+
+    float worldMat[16] = {0};
+    worldMat[0] = 1;
+    worldMat[5] = 1;
+    worldMat[10] = 1;
+    worldMat[15] = 1;
+
+    worldMat[12] = -translate_offset.x;
+    worldMat[13] = -translate_offset.y;
+    worldMat[14] = -translate_offset.z;
+    glUniformMatrix4fv(worldTransUni, 1, GL_FALSE, worldMat);
+
+    float transformMat[16] = {0};
+    float angle = y_rot_angle * (M_PI / 180);
     transformMat[0] = cos(angle);
-    transformMat[8] = sin(angle);
-
-    transformMat[5] = 1;
-
     transformMat[2] = -sin(angle);
+    transformMat[5] = 1;
+    transformMat[8] = sin(angle);
     transformMat[10] = cos(angle);
-
     transformMat[15] = 1;
-    glUniformMatrix4fv(transformMatUnif, 1, GL_FALSE, transformMat);
+    glUniformMatrix4fv(rotateTransUni, 1, GL_FALSE, transformMat);
+
+    // recover the lookat vector
+    //vec3 lookat = calc_lookat(translate_offset, y_rot_angle);
+    vec3 lookat = {0, 0, -1};
+    vec3 up = {0, 1, 0};
+    vec3 up_down_axis = cross(lookat, up);
+    //printf("x=%f y=%f z=%f\n", up_down_axis.x, up_down_axis.y, up_down_axis.z);
+    // rotate around that axis
+    angle = x_rot_angle * (M_PI / 180);
+    //printf("xrotangle=%f\n", x_rot_angle);
+    mat4 x_rot_mat = rotate(angle, normalize3(up_down_axis));
+    glUniformMatrix4fv(xRotateTransUni, 1, GL_FALSE, x_rot_mat.value);
 
     glBindBuffer(GL_ARRAY_BUFFER, pos_buffer_obj);
 
@@ -117,10 +245,10 @@ void handle_key(unsigned char key, int x, int y) {
     }
     switch (key) {
         case 'e':
-            rot_angle += 1.0f;
+            y_rot_angle += 1.0f;
             break;
         case 'r':
-            rot_angle -= 1.0f;
+            y_rot_angle -= 1.0f;
             break;
     }
 }
@@ -130,10 +258,12 @@ void handle_special(int key, int x, int y) {
 
     switch (key) {
         case GLUT_KEY_HOME:
-            zoom += step;
+            translate_offset.z -= step;
+            // zoom += step;
             break;
         case GLUT_KEY_END:
-            zoom = fmax(0, zoom - step);
+            translate_offset.z += step;
+            // zoom = fmax(0, zoom - step);
             break;
         case GLUT_KEY_LEFT:
             translate_offset.x -= step;
@@ -148,6 +278,19 @@ void handle_special(int key, int x, int y) {
             translate_offset.y -= step;
             break;
     }
+}
+
+int lx = 0, ly = 0;
+int screen_width;
+int screen_height;
+void mouse_movement(int x, int y) {
+    if (lx != 0 || ly != 0) {
+        y_rot_angle += (float)(x - lx) / (float)(screen_width) * 365;
+        x_rot_angle += (float)(y - ly) * 20;
+    }
+
+    lx = x;
+    ly = y;
 }
 
 void changeSize(int w, int h) {
@@ -216,6 +359,9 @@ int main(int argc, char **argv) {
     glutInit(&argc, argv);
     glutInitWindowSize(320, 320);
     glutCreateWindow("Fantasy Golf Simulator");
+    screen_width = glutGet(GLUT_SCREEN_WIDTH);
+    screen_height = glutGet(GLUT_SCREEN_HEIGHT);
+    glutPassiveMotionFunc(mouse_movement);
 
     GLenum err = glewInit();
     if (err != GLEW_OK) {
@@ -249,10 +395,12 @@ int main(int argc, char **argv) {
 
     offsetUniform = glGetUniformLocation(shader_program, "offset");
     perspectiveMatrixUnif = glGetUniformLocation(shader_program, "perspectiveMatrix");
-    transformMatUnif = glGetUniformLocation(shader_program, "transform");
+    rotateTransUni = glGetUniformLocation(shader_program, "rotate_trans");
+    xRotateTransUni = glGetUniformLocation(shader_program, "x_rotate_trans");
+    worldTransUni = glGetUniformLocation(shader_program, "world_trans");
     zoom_uniform = glGetUniformLocation(shader_program, "zoom");
 
-    float fFrustumScale = 1.0f; float fzNear = 0.5f; float fzFar = 3.0f;
+    float fFrustumScale = 1.0f; float fzNear = 0.025f; float fzFar = 10.0f;
 
     float theMatrix[16];
     memset(theMatrix, 0, sizeof(float) * 16);
