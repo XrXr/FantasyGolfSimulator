d24b407720210f51674c93ca7e41a49184158099
XrXr <xrsquared@gmail.com>
2016-08-03 11:59:12 -0400

    Move font code into a separate compilation unit

---
+++ font.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stdint.h>
+#include <GL/glew.h>
+
+void render_char(const char c, const float x, const float y,
+                 size_t* buf_offset);
+void render_string(const char* s, float x, float y);
+GLuint font_init(void);
